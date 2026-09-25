15451718f3254fd2f39c54d572218311c2eaa194
vladlenas <54670862+vladlenas@users.noreply.github.com>
2026-09-26 00:04:27 +0300

    Update helper.py

--- src/helper/helper.py
+++ src/helper/helper.py
@@ -770,26 +770,26 @@ class Handler(BaseHTTPRequestHandler):
 
             return
 
-       if path == "/restart":
-    ok, message = restart_package()
+            if path == "/restart":
+            ok, message = restart_package()
 
-    if ok:
-        self.redirect("/")
-    else:
-        self.send_html(
-            page_header("Restart Error")
-            + """
+            if ok:
+                self.redirect("/")
+            else:
+                self.send_html(
+                    page_header("Restart Error")
+                    + """
 <div class="card">
 <h1>Restart failed</h1>
 <p>{}</p>
 </div>
 """.format(html.escape(message))
-            + page_footer(),
-            500,
-        )
+                    + page_footer(),
+                    500,
+                )
 
-    return
-
+            return
+            
         self.send_html("Not Found", 404)
 
     def log_message(self, format_string, *args):
